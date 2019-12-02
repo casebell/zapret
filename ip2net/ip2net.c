c9b8624a458053189e426647d1c408ddfb05efa1
bol-van <k@vodka.home.kg>
2019-12-02 10:51:58 +0300

    ip2net : statics

--- ip2net/ip2net.c
+++ ip2net/ip2net.c
@@ -26,7 +26,7 @@
 // must be no less than N ipv6 in subnet
 #define DEFAULT_V6_THRESHOLD	5
 
-int ucmp(const void * a, const void * b, void *arg)
+static int ucmp(const void * a, const void * b, void *arg)
 {
 	if (*(uint32_t*)a < *(uint32_t*)b)
 		return -1;
@@ -35,13 +35,13 @@ int ucmp(const void * a, const void * b, void *arg)
 	else
 		return 0;
 }
-uint32_t mask_from_bitcount(uint32_t zct)
+static uint32_t mask_from_bitcount(uint32_t zct)
 {
 	return ~((1 << zct) - 1);
 }
 // make presorted array unique. return number of unique items.
 // 1,1,2,3,3,0,0,0 (ct=8) => 1,2,3,0 (ct=4)
-uint32_t unique(uint32_t *pu, uint32_t ct)
+static uint32_t unique(uint32_t *pu, uint32_t ct)
 {
 	uint32_t i, j, u;
 	for (i = j = 0; j < ct; i++)
@@ -55,7 +55,7 @@ uint32_t unique(uint32_t *pu, uint32_t ct)
 
 
 
-int cmp6(const void * a, const void * b, void *arg)
+static int cmp6(const void * a, const void * b, void *arg)
 {
 	for (uint8_t i = 0; i < sizeof(((struct in6_addr *)0)->s6_addr); i++)
 	{
@@ -67,7 +67,7 @@ int cmp6(const void * a, const void * b, void *arg)
 	return 0;
 }
 // make presorted array unique. return number of unique items.
-uint32_t unique6(struct in6_addr *pu, uint32_t ct)
+static uint32_t unique6(struct in6_addr *pu, uint32_t ct)
 {
 	uint32_t i, j, k;
 	for (i = j = 0; j < ct; i++)
@@ -77,7 +77,7 @@ uint32_t unique6(struct in6_addr *pu, uint32_t ct)
 	}
 	return i;
 }
-void mask_from_bitcount6(uint32_t zct, struct in6_addr *a)
+static void mask_from_bitcount6(uint32_t zct, struct in6_addr *a)
 {
 	if (zct > 128) zct = 128;
 	int32_t n = zct < 128 ? (127 - zct) >> 3 : -1;
@@ -85,21 +85,21 @@ void mask_from_bitcount6(uint32_t zct, struct in6_addr *a)
 		a->s6_addr[i] = i > n ? 0x00 : i < n ? 0xFF : ~((1 << (zct & 7)) - 1);
 }
 // result = a & b
-void ip6_and(const struct in6_addr *a, const struct in6_addr *b, struct in6_addr *result)
+static void ip6_and(const struct in6_addr *a, const struct in6_addr *b, struct in6_addr *result)
 {
 	for (uint8_t i = 0; i < (sizeof(a->s6_addr) / sizeof(uint32_t)); i++)
 		((uint32_t*)result->s6_addr)[i] = ((uint32_t*)a->s6_addr)[i] & ((uint32_t*)b->s6_addr)[i];
 }
 
 
-void rtrim(char *s)
+static void rtrim(char *s)
 {
 	if (s)
 		for (char *p = s + strlen(s) - 1; p >= s && (*p == '\n' || *p == '\r'); p--) *p = '\0';
 }
 
 
-struct params_s
+static struct params_s
 {
 	bool ipv6;
 	uint32_t pctmult, pctdiv; // for v4
@@ -108,7 +108,7 @@ struct params_s
 } params;
 
 
-void exithelp()
+static void exithelp()
 {
 	printf(
 		" -4\t\t\t\t; ipv4 list (default)\n"
@@ -120,7 +120,7 @@ void exithelp()
 	exit(1);
 }
 
-void parse_params(int argc, char *argv[])
+static void parse_params(int argc, char *argv[])
 {
 	int option_index = 0;
 	int v, i;
